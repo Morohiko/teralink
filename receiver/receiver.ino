e3858d60f9456539fcbe966e2bb899c9b48ac132
Morohiko <qkatashi@gmail.com>
2024-09-27 12:18:01 +0300

    add receiver lora communication

---
+++ receiver/receiver.ino
@@ -0,0 +1,37 @@
+#include <SPI.h>
+#include <LoRa.h>
+
+// LoRa pins for Arduino Nano
+#define ss 10    // NSS
+#define rst 9    // RST
+#define dio0 2   // DIO0
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial);
+
+  Serial.println("LoRa Receiver");
+
+  LoRa.setPins(ss, rst, dio0);
+
+  if (!LoRa.begin(433E6)) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
+}
+
+void loop() {
+  int packetSize = LoRa.parsePacket();
+  if (packetSize) {
+    Serial.print("Received packet: ");
+
+    while (LoRa.available()) {
+      String received = LoRa.readString();
+      Serial.print(received);
+    }
+
+    Serial.print(" with RSSI: ");
+    Serial.println(LoRa.packetRssi());
+  }
+}
+
